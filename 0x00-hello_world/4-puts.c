a0a81ddc12d5f5e24b862da834f61821d2f7f329
Eden <edeniyanda@gmail.com>
2022-09-09 03:52:45 -0700

    101-quote.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,19 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- * main - prints the string in the put function
- * Description using the main functon
- * this program prints "Programming is like building a multilingual puzzule"
- * Return: 0 if exited properly, non-zero otherwise
+ *
+ * main - Write '"Programming is like building a multilingual puzzle'
+ *
+ * *Return: Always 0 (success)
+ *
  */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzule");
+
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }

5fac6432b1af2f8e388f4225bf0ab6d0970e88a5
Eden <edeniyanda@gmail.com>
2022-09-09 02:31:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <studio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzule
+ * main - prints the string in the put function
+ * Description using the main functon
+ * this program prints "Programming is like building a multilingual puzzule"
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)

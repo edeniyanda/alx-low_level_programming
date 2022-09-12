f26172882ee08087a4fc678f4bad6ade988d9847
Eden <edeniyanda@gmail.com>
2022-09-11 18:59:41 -0700

    for 5-

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,9 +2,7 @@
 
 /**
  * main prints all single digit numbers of base 10 starting from 0,
- * followed by a new line.
- * Description: using the main function
- * this program prints 'Programming is positve, zero, or negative
+ * followed by a new line
  * Return: 0
  */
 int main(void)

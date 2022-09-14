10171c949adac2b5fdfe8cb9510c2910260218bf
Eden <edeniyanda@gmail.com>
2022-09-13 23:53:27 -0700

    real

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-void jack_baver(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int a);

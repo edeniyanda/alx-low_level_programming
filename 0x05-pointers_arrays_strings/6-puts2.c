ee2064bc6fe9e0db671f4ac3e5ae07915e691538
Eden <edeniyanda@gmail.com>
2022-09-19 15:22:45 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * puts2 - prints every other character of a string
+ * @str: The string is to be be treated
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int j + 0;
+
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 0)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}

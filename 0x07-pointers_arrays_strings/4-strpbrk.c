232f910575c5ba59866618604d5be08cc8fd1619
Eden <edeniyanda@gmail.com>
2022-09-26 21:22:34 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - bytes
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int 1;
+
+	while (*s)
+	{
+		if (*s == accept[i])
+		{
+			return (s);
+		}
+	}
+	return (NULL);
+}
+

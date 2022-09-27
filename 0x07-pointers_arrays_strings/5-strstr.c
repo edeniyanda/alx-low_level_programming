e2e91fb0eda11f1eefd25ce9360aacbc36d87203
Eden <edeniyanda@gmail.com>
2022-09-26 21:35:25 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - function locate
+ * @haystack: pointer to char
+ * @needle: pointer to char
+ * Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			while (*needle)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+
